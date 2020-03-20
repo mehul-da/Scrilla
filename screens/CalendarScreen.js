fbd50b182886b03b3fd860ed65738ac9881c4ca2
Mehul Daruka <mehuldaruka32@gmail.com>
2020-03-20 18:09:41 -0500

    Updated the Calendar screen.

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,11 +1,35 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ALert, Alert } from 'react-native';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 export default class CalendarScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: ""
+        }
+    }
+
     render() {
         return (
             <View>
-                <Text>CALENDAR SCREEN</Text>
+                <View style = {{alignItems: 'center', paddingTop: 40}}>
+                    <Text style = {{fontSize: 25, fontWeight: "bold", fontFamily: "Trebuchet MS"}}>Calendar</Text>
+                </View>
+                <View style = {{paddingTop: 30, padding: 15}}>
+                    <Calendar
+                    minDate = {'2020-03-01'}
+                    onDayPress = {(day) => {
+                        console.log(day)
+                        this.setState({date: `${day.month}/${day.day}/${day.year}`})
+                        }}
+                    onDayLongPress = {(day) => {
+                        this.setState({date: ""})
+                    }}/>
+                    <View style = {{alignItems: 'center', paddingTop: 20}}>
+                        <Text style = {{fontFamily: "Trebuchet MS", fontSize: 20}}>{this.state.date}</Text>
+                    </View>
+                </View>
             </View>
         );
     }
