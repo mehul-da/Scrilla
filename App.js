2ae1fbc17ea543e515ba5f8ff76f6a2b7cd941bd
Mehul Daruka <mehuldaruka32@gmail.com>
2020-03-20 15:46:53 -0500

    Updated the task bar

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from 'react-native-vector-icons';
+import { FontAwesome } from 'react-native-vector-icons';
+import { FontAwesome5 } from 'react-native-vector-icons';
 
 function HomeScreen() {
   return (
@@ -41,10 +44,34 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName = "Home" shifting = {false}>
-        <Tab.Screen name="Home" component={HomeScreen}/>
-        <Tab.Screen name="Calendar" component={CalendarScreen}/>
-        <Tab.Screen name="Transaction Hist" component={TransactionHistoryScreen} />
-        <Tab.Screen name="Add Trans" component={AddTransactionScreen}/>
+        <Tab.Screen
+        name="Home" 
+        component={HomeScreen}
+        options = {{
+          tabBarIcon: () =>
+          <FontAwesome5 name = "home" size = {23}/>
+        }}/>
+        <Tab.Screen 
+        name="Calendar" 
+        component={CalendarScreen}
+        options = {{
+          tabBarIcon: () =>
+          <FontAwesome name = "calendar" size = {23}/>
+        }}/>
+        <Tab.Screen 
+        name="History" 
+        component={TransactionHistoryScreen} 
+        options = {{
+          tabBarIcon: () =>
+          <FontAwesome name = "history" size = {25}/>
+        }}/>
+        <Tab.Screen 
+        name="New Trans." 
+        component={AddTransactionScreen}
+        options = {{
+          tabBarIcon: () =>
+          <FontAwesome name = "plus" size = {23}/>
+        }}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
